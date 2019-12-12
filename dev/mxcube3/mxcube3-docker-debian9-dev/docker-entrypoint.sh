8fe32e9651b9ac8ada6cf0a1692c447422be898d
Laís Pessine do Carmo <lais.carmo@lnls.br>
2019-12-12 10:33:16 -0300

    Add commented initialization lnls_sol and lnls_manaca configs
    The container as it is initialized with the default (mockup) config.
    Initialization with lnls_sol and lnls_manaca configs are added as
    comments (as a hint).

--- dev/mxcube3/mxcube3-docker-debian9-dev/docker-entrypoint.sh
+++ dev/mxcube3/mxcube3-docker-debian9-dev/docker-entrypoint.sh
@@ -10,5 +10,7 @@ vncserver :1 -geometry 1680x1050 -depth 24 &
 
 redis-server&
 python3 mxcube3-server -r test/HardwareObjectsMockup.xml --log-file mxcube.log&
+#python3 mxcube3-server -r mxcube3/HardwareRepository/configuration/lnls_sol --log-file mxcube_lnls_sol.log&
+#python3 mxcube3-server -r mxcube3/HardwareRepository/configuration/lnls_manaca --log-file mxcube_lnls_manaca.log&
 npm start&
 wait
