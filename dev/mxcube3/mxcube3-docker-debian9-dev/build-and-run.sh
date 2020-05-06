ac8dd7833c325813aed73625952912eb7efbe85c
Laís Pessine do Carmo <lais.carmo@lnls.br>
2020-05-06 19:29:01 -0300

    Fix mxcube3 clone step

--- dev/mxcube3/mxcube3-docker-debian9-dev/build-and-run.sh
+++ dev/mxcube3/mxcube3-docker-debian9-dev/build-and-run.sh
@@ -1,16 +1,29 @@
 echo "Build and run mxcube3 from the start"
 
+echo " "
 echo "Remove previous mxcube3-dev-lnls container"
+echo " "
 docker rm -f mxcube3-dev-lnls
 
+echo " "
 echo "Build mxcube3-dev-lnls"
+echo " "
 docker build --network=host -t mxcube3-dev-lnls .
 
-# The following lines download mxcube3 source (if not present locally yet).
-# mxcube3 folder will be mounted inside the final container:
-echo " "
-echo "Clone mxcube (if needed)"
-. debian-install.sh && mxcube_download
+# The following lines download mxcube3 source in the current folder (if not
+# present locally yet).
+# mxcube3 folder will be mounted inside the final container.
+if [ -d "mxcube3" ]; then
+  echo " "
+  echo "Mxcube3 folder exists. Skip clone."
+  echo " "
+else
+  echo " "
+  echo "Mxcube3 folder do not exist. Clone mxcube3."
+  echo " "
+  . debian-install.sh && mxcube_download
+fi
+
 
 docker run -p 5901:5901 -p 8090:8090 --network=host -dt \
 -v ${PWD}/mxcube3:/opt/mxcube3/ \
