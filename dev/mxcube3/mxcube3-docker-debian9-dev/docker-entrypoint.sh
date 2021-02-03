4519f8e54a95a9977b3561f2e1c716d9c06be643
Lais Pessine do Carmo <lais.carmo@lnls.br>
2021-02-03 17:52:22 -0300

    Update minor comments and IPs

--- dev/mxcube3/mxcube3-docker-debian9-dev/docker-entrypoint.sh
+++ dev/mxcube3/mxcube3-docker-debian9-dev/docker-entrypoint.sh
@@ -12,16 +12,15 @@ cd /opt/mxcube3
 npm start&
 redis-server&
 
-# mock
+# MOCK
 python3 mxcube3-server -w -r test/HardwareObjectsMockup.xml --log-file mxcube.log&
 
-# sol
+# SOL
 #python3 mxcube3-server -w -r mxcube3/HardwareRepository/configuration/lnls_sol --log-file mxcube_lnls_sol.log&
 
-# mnc
+# MNC
 #export HDF5_USE_FILE_LOCKING='FALSE'
-#export EPICS_CA_ADDR_LIST="10.31.74.17 10.31.74.32 10.31.74.24 10.31.74.33 10.31.74.40 10.31.74.41 10.31.74.23 10.31.74.45 10.0.38.46:60000 10.0.38.59:60000" 
- 
+#export EPICS_CA_ADDR_LIST="10.10.10.41 10.10.10.42 10.10.10.49 10.31.74.17 10.31.74.32 10.31.74.24 10.31.74.33 10.31.74.40 10.31.74.41 10.31.74.23 10.31.74.45 10.0.38.46:60000 10.0.38.59:60000"  
 #python3 mxcube3-server -w -r mxcube3/HardwareRepository/configuration/lnls_manaca --log-file mxcube_lnls_manaca.log&
 
 wait
