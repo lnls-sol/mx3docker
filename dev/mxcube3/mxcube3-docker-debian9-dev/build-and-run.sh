0261bbe7a0a014b036edc8c485aaabc93a243627
Lais Pessine do Carmo <lais.carmo@lnls.br>
2021-03-08 15:05:54 -0300

    Fix -v lines

--- dev/mxcube3/mxcube3-docker-debian9-dev/build-and-run.sh
+++ dev/mxcube3/mxcube3-docker-debian9-dev/build-and-run.sh
@@ -29,9 +29,9 @@ docker run -p 5901:5901 -p 8090:8090 --network=host -dt \
 -v ${PWD}/mxcube3:/opt/mxcube3/ \
 -v ${PWD}/docker-entrypoint.sh:/usr/local/bin/docker-entrypoint.sh:rw \
 -v /ibira/sirius/beamlines/manaca/:/ibira/sirius/beamlines/manaca/ \
--v /usr/local/sol/:/usr/local/sol/
--v /usr/local/setup-bl:/usr/local/setup-bl
--v /usr/local/sol-nfs:/usr/local/sol-nfs
+-v /usr/local/sol/:/usr/local/sol/ \
+-v /usr/local/setup-bl:/usr/local/setup-bl \
+-v /usr/local/sol-nfs:/usr/local/sol-nfs \
 --privileged \
 --name mxcube3-dev-lnls \
 mxcube3-dev-lnls
